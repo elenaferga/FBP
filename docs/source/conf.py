31e7ab5b6c5b68dba44076bc41dbd2e3b7637bb4
elenaferga <87361846+elenaferga@users.noreply.github.com>
2024-02-14 12:25:18 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,8 +21,16 @@ extensions = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
+    'matplotlib': ('http://matplotlib.org/', None),
+    'astropy': ('http://docs.astropy.org/en/stable/', None),
+    'h5py': ('http://docs.h5py.org/en/latest/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
+
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
@@ -30,6 +38,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+pygments_style = 'sphinx'
+
+# If true, keep warnings as "system message" paragraphs in the built documents.
+keep_warnings = True
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
